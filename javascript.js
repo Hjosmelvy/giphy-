1ffc58afae003303815e4167d54a693ffff03f6f
hjosmelvy <hjosmelvy@yahoo.com>
2017-01-12 22:59:41 +0000

    Incomplete, MTA API added

--- javascript.js
+++ javascript.js
@@ -2,28 +2,14 @@
 /* global $ */
 $(document).ready(function() {
 
-    $("#userButton").click(function(){
-        
-    
-        var userSearch=$("#userSearch").val();
-    
-   $.getJSON(
-     
-      "https://api.giphy.com/v1/gifs/search?q="+userSearch +"&api_key=dc6zaTOxFJmzC",
-     
-     
-      function(response) {
-        console.log(response);
-for(var i=0;i<5;i++){
-               $("#gif").append("<img class='img' src=" + response.data[i].images.fixed_height.url + ">");
-       
-      
-        
-}
-      }); 
-});
-$("#reset").click(function(){
-    $(".img").remove();
-});
 
+    var userDemand="stops";
+    
+   $.getJSON("http://ajar-target.gomix.me/"+userDemand,function(response) {
+        console.log(response);
+        
+        var stops= Object.keys("http://ajar-target.gomix.me/stops");
+        
+        
+});
 });
