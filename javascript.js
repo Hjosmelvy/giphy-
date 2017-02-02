536cc0f1730faff01011371c85a56c9a69d94b3e
hjosmelvy <hjosmelvy@yahoo.com>
2017-02-02 23:19:31 +0000

    Works

--- javascript.js
+++ javascript.js
@@ -3,21 +3,47 @@
 $(document).ready(function() {
 
 
-    
-    
-  $.getJSON("https://ajar-target.gomix.me/status",function(response) {
-      //  console.log(response);
-        
-        //var stops= Object.getOwnPropertyNames("http://ajar-target.gomix.me/stops");
-       console.log(Object.keys(response));
-        
-    
-for( var i=0; i<response.length;i++){
-        $("#info").append(response[i].name+" : "+response[i].status + "<br>");
-}
-        
-});
-        
-});
 
 
+  $.getJSON("https://ajar-target.gomix.me/status", function(response) {
+    //  console.log(response);
+
+    //var stops= Object.getOwnPropertyNames("http://ajar-target.gomix.me/stops");
+    console.log(Object.keys(response));
+      var listOfStops = [];
+        var listOfTrains=[];
+       for (var i=0; i<response.length; i++){
+        
+         listOfTrains.push(response[i].name);
+        
+         
+       }
+       $("#userButton").click(function(){
+
+       for(i=0;i<listOfTrains.length;i++){
+        
+         if( listOfTrains[i].includes($("#userSearch").val().toUpperCase()) ){
+          console.log(response[i].name+":"+response[i].status);
+             $("#info").append(response[i].name + " : " + response[i].status + "<br>");
+         } 
+       }
+       });
+  
+
+
+
+    
+    
+     console.log(listOfTrains);
+
+
+
+
+
+
+
+
+
+  });
+
+});
